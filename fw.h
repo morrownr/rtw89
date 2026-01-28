43c7246c2ea78cfab61f3a3eff8e0fcb25fd4338
Ping-Ke Shih <pkshih@realtek.com>
2026-01-28 23:49:09 +0800

    wifi: rtw89: fw: add CMAC H2C command for punctured for RTL8922D
    CMAC H2C command is to configure CMAC such as TX retry and padding. Add to
    update CMAC function block while puncturing is changing.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20260106030911.15528-9-pkshih@realtek.com

--- fw.h
+++ fw.h
@@ -5057,6 +5057,9 @@ int rtw89_fw_h2c_punctured_cmac_tbl_g7(struct rtw89_dev *rtwdev,
 				       struct rtw89_vif_link *rtwvif_link,
 				       u16 punctured);
 #endif
+int rtw89_fw_h2c_punctured_cmac_tbl_be(struct rtw89_dev *rtwdev,
+				       struct rtw89_vif_link *rtwvif_link,
+				       u16 punctured);
 int rtw89_fw_h2c_txpath_cmac_tbl(struct rtw89_dev *rtwdev,
 				 struct rtw89_sta_link *rtwsta_link);
 int rtw89_fw_h2c_update_beacon(struct rtw89_dev *rtwdev,
